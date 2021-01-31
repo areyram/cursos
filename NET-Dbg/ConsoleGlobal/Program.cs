428c60e12423c09c48efd3c4dad64ff0c0a59ada
Alejandro Reyes <areyes@ventus-tech.com>
2021-01-30 20:57:48 -0600

    NET-GDI

---
+++ NET-Dbg/ConsoleGlobal/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleGlobal
+{
+    class Program
+    {
+        static void Main()
+        {
+            AppDomain.CurrentDomain.UnhandledException += ErrorHandler;
+            Falla();
+            Console.WriteLine("Bye.");
+        }
+
+        private static void Falla()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ErrorHandler(object sender, UnhandledExceptionEventArgs e)
+        {
+            Console.WriteLine("Oops... error. Whatever.");
+        }
+    }
+}
---
+++ NET-Dbg/WindowsGlobal/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace sample1
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.ThreadException += ErrorHandler;
+            Application.Run(new Form1());
+        }
+
+        private static void ErrorHandler(object sender, ThreadExceptionEventArgs e)
+        {
+            if (MessageBox.Show("Oops... algo salió mal. Nos vamos o que?", "Error",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Error) == DialogResult.Yes)
+                Application.Exit();
+            else if (MessageBox.Show("Quieres ver el mensaje de error?", "Confirmación",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                MessageBox.Show(e.Exception.ToString());
+        }
+    }
+}
